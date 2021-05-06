b5cbb5aee1cc5cb00cf5d837c8e759e4ab5d8375
AxmetES <fraktsia@gmail.com>
2021-05-06 15:36:06 +0600

    Step 9 - 14. - Added random animation for starts.

--- main.py
+++ main.py
@@ -9,40 +9,41 @@ async def blink(canvas, row, column, symbol='*'):
     while True:
         canvas.addstr(row, column, symbol, curses.A_DIM)
         canvas.refresh()
-        for _ in range(1):
+        for _ in range(random.randint(2, 3)):
             await asyncio.sleep(0)
 
         canvas.addstr(row, column, symbol)
         canvas.refresh()
-        for _ in range(1):
+        for _ in range(random.randint(1, 3)):
             await asyncio.sleep(0)
 
         canvas.addstr(row, column, symbol, curses.A_BOLD)
         canvas.refresh()
-        for _ in range(3):
+        for _ in range(random.randint(2, 4)):
             await asyncio.sleep(0)
 
         canvas.addstr(row, column, symbol)
         canvas.refresh()
-        for _ in range(1):
+        for _ in range(random.randint(1, 3)):
             await asyncio.sleep(0)
 
 
 def draw(canvas):
     canvas.border()
-    symbols = '..+..*..:..'
+    symbols = '..+..:::..'
     x, y = window.getmaxyx()
     coroutines = [(blink(canvas, row=randint(0, x - 2), column=randint(0, y - 2), symbol=random.choice(symbols))) for _
-                  in range(500)]
+                  in range(100)]
 
     while True:
         for coroutine in coroutines:
             coroutine.send(None)
-        time.sleep(0.1)
+        time.sleep(0.2)
 
 
 if __name__ == '__main__':
     curses.update_lines_cols()
     window = curses.initscr()
+    curses.curs_set(False)
     curses.wrapper(draw)
-    curses.curs_set(True)
+
