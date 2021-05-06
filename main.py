40ea78ebd50ccfbc654d6d0366869113dbfeecf5
AxmetES <fraktsia@gmail.com>
2021-05-06 20:22:34 +0600

    Step 10-14. - Added projectile.

--- main.py
+++ main.py
@@ -5,6 +5,36 @@ import time
 from random import randint
 
 
+async def fire(canvas, start_row, start_column, rows_speed=-0.3, columns_speed=0):
+    """Display animation of gun shot, direction and speed can be specified."""
+
+    row, column = start_row, start_column
+
+    canvas.addstr(round(row), round(column), '*')
+    await asyncio.sleep(0)
+
+    canvas.addstr(round(row), round(column), 'O')
+    await asyncio.sleep(0)
+    canvas.addstr(round(row), round(column), ' ')
+
+    row += rows_speed
+    column += columns_speed
+
+    symbol = '-' if columns_speed else '|'
+
+    rows, columns = canvas.getmaxyx()
+    max_row, max_column = rows - 1, columns - 1
+
+    curses.beep()
+
+    while 0 < row < max_row and 0 < column < max_column:
+        canvas.addstr(round(row), round(column), symbol)
+        await asyncio.sleep(0)
+        canvas.addstr(round(row), round(column), ' ')
+        row += rows_speed
+        column += columns_speed
+
+
 async def blink(canvas, row, column, symbol='*'):
     while True:
         canvas.addstr(row, column, symbol, curses.A_DIM)
@@ -35,9 +65,16 @@ def draw(canvas):
     coroutines = [(blink(canvas, row=randint(0, x - 2), column=randint(0, y - 2), symbol=random.choice(symbols))) for _
                   in range(100)]
 
+    x_middle = x // 2
+    y_middle = y // 2
+    time.sleep(20)
+    coroutines.append(fire(canvas, start_row=x_middle, start_column=y_middle))
     while True:
-        for coroutine in coroutines:
-            coroutine.send(None)
+        for coroutine in coroutines.copy():
+            try:
+                coroutine.send(None)
+            except StopIteration:
+                coroutines.remove(coroutine)
         time.sleep(0.2)
 
 
@@ -46,4 +83,3 @@ if __name__ == '__main__':
     window = curses.initscr()
     curses.curs_set(False)
     curses.wrapper(draw)
-
