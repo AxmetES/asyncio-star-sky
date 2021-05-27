ead0c647a429ac5684944977fd236ad368921ade
axmetES <fraktsia@gmail.com>
2021-05-27 15:48:57 +0600

    Review 4-4. Renamed incomprehensible var and func. Deleted extra refresh() func.

--- curses_tools.py
+++ curses_tools.py
@@ -38,8 +38,6 @@ def read_controls(canvas):
 
 
 def draw_frame(canvas, start_row, start_column, text, negative=False):
-    # """Draw multiline text fragment on canvas, erase text instead of drawing if negative=True is specified."""
-
     rows_number, columns_number = canvas.getmaxyx()
 
     for row, line in enumerate(text.splitlines(), round(start_row)):
@@ -59,10 +57,6 @@ def draw_frame(canvas, start_row, start_column, text, negative=False):
 
             if symbol == ' ':
                 continue
-
-            # Check that current position it is not in a lower right corner of the window
-            # Curses will raise exception in that case. Don`t ask why…
-            # https://docs.python.org/3/library/curses.html#curses.window.addch
             if row == rows_number - 1 and column == columns_number - 1:
                 continue
 
@@ -71,8 +65,6 @@ def draw_frame(canvas, start_row, start_column, text, negative=False):
 
 
 def get_frame_size(text):
-    # """Calculate size of multiline text fragment, return pair — number of rows and colums."""
-
     lines = text.splitlines()
     rows = len(lines)
     columns = max([len(line) for line in lines])
--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from random import randint
 from curses_tools import draw_frame, read_controls, get_frame_size
 
 
-def get_file(filename):
+def get_image(filename):
     with open(filename, 'r') as file:
         file_context = file.read()
     return file_context
@@ -16,28 +16,28 @@ def get_file(filename):
 
 async def starship_animation(canvas, start_row, start_column, images):
     for image in cycle(images):
-        frame_row, frame_col = get_frame_size(image)
+        image_row, image_col = get_frame_size(image)
         rows_number, columns_number = canvas.getmaxyx()
 
-        row_bottom = rows_number - frame_row
-        col_right = columns_number - frame_col
+        row_bottom = rows_number - image_row
+        col_right = columns_number - image_col
 
-        rows_direction, columns_direction, space_pressed = read_controls(canvas)
-        start_row = start_row + rows_direction
-        start_column = start_column + columns_direction
+        rows_direct, columns_direct, space_pressed = read_controls(canvas)
+        start_row = start_row + rows_direct
+        start_column = start_column + columns_direct
 
-        if max(start_row, frame_row) == frame_row:
+        if max(start_row, image_row) == image_row:
             start_row = 0
         if min(start_row, row_bottom) == row_bottom:
             start_row = row_bottom
-        if max(start_column, frame_col) == frame_col:
+        if max(start_column, image_col) == image_col:
             start_column = 0
         if min(start_column, col_right) == col_right:
             start_column = col_right
 
         draw_frame(canvas, start_row, start_column, image, negative=False)
         canvas.refresh()
-        for _ in range(0, 2):
+        for _ in range(0, 1):
             await asyncio.sleep(0)
             draw_frame(canvas, start_row, start_column, image, negative=True)
 
@@ -48,22 +48,18 @@ async def blink(canvas, row, column, symbol='*'):
 
     while True:
         canvas.addstr(row, column, symbol, curses.A_DIM)
-        canvas.refresh()
         for _ in range(1, 5):
             await asyncio.sleep(0)
 
         canvas.addstr(row, column, symbol)
-        canvas.refresh()
         for _ in range(1, 2):
             await asyncio.sleep(0)
 
         canvas.addstr(row, column, symbol, curses.A_BOLD)
-        canvas.refresh()
         for _ in range(1, 5):
             await asyncio.sleep(0)
 
         canvas.addstr(row, column, symbol)
-        canvas.refresh()
         for _ in range(1, 2):
             await asyncio.sleep(0)
 
@@ -71,17 +67,19 @@ async def blink(canvas, row, column, symbol='*'):
 def draw(canvas):
     canvas.border()
     symbols = '..+..:::..'
-    img_1 = get_file('rocket_frame_1.txt')
-    img_2 = get_file('rocket_frame_2.txt')
+    img_1 = get_image('rocket_frame_1.txt')
+    img_2 = get_image('rocket_frame_2.txt')
     images = [img_1, img_2]
     y, x = window.getmaxyx()
-    coroutines = [(blink(canvas, row=randint(0, y - 2), column=randint(0, x - 2), symbol=random.choice(symbols))) for _
-                  in range(100)]
+    coroutines = [(blink(canvas, row=randint(0, y - 2),
+                         column=randint(0, x - 2),
+                         symbol=random.choice(symbols))) for _ in range(100)]
 
     row_middle = (y // 2) - 3
     col_middle = (x // 2) - 3
 
-    coroutines.append(starship_animation(canvas, row_middle, col_middle, images))
+    coroutines.append(starship_animation(
+        canvas, row_middle, col_middle, images))
 
     while True:
         for coroutine in coroutines.copy():
